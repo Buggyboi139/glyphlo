6a815ae38d29f724b4c21ad277097fb95b989fa2
Buggyboi139 <willworkforbugs@gmail.com>
2026-03-15 21:26:31 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'glyphlo-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install the service worker and cache the assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Intercept network requests and serve from cache if available
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Return cached version if found, otherwise fetch from the network
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
